53337675c691be91bff4413e3d9a9478c9ea5c21
Yevhen Kliuchyk <kliuchyk@gmail.com>
2024-05-30 16:04:17 +0200

    add n-progress bar

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,14 +1,22 @@
 import { Container, Menu, Image, Icon } from "semantic-ui-react";
 import Link from "next/link";
+import Router, { useRouter } from "next/router";
+import NProgress from "nprogress";
+
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 function Header() {
   const user = false;
+  const router = useRouter();
+  const isActive = (route) => router.pathname === route;
 
   return (
     <Menu fluid id="menu" inverted>
       <Container text>
         <Link href="/">
-          <Menu.Item header>
+          <Menu.Item header active={isActive("/")}>
             <Image
               size="mini"
               src="/static/logo.svg"
@@ -18,7 +26,11 @@ function Header() {
           </Menu.Item>
         </Link>
         <Link href="/cart">
-          <Menu.Item header>
+          <Menu.Item
+            header
+            active={isActive("/cart")}
+            style={{ height: "100%" }}
+          >
             <Icon name="cart" size="large" />
             Cart
           </Menu.Item>
@@ -27,18 +39,26 @@ function Header() {
         {user ? (
           <>
             <Link href="/create">
-              <Menu.Item header>
+              <Menu.Item
+                header
+                active={isActive("/create")}
+                style={{ height: "100%" }}
+              >
                 <Icon name="add square" size="large" />
                 Create
               </Menu.Item>
             </Link>
             <Link href="/account">
-              <Menu.Item header>
+              <Menu.Item
+                header
+                active={isActive("/account")}
+                style={{ height: "100%" }}
+              >
                 <Icon name="user" size="large" />
                 Account
               </Menu.Item>
             </Link>
-            <Menu.Item header>
+            <Menu.Item header style={{ height: "100%" }}>
               <Icon name="sign out" size="large" />
               Logout
             </Menu.Item>
@@ -46,13 +66,22 @@ function Header() {
         ) : (
           <>
             <Link href="/login">
-              <Menu.Item header>
+              <Menu.Item
+                header
+                active={isActive("/login")}
+                style={{ height: "100%" }}
+              >
                 <Icon name="sign in" size="large" />
                 Login
               </Menu.Item>
             </Link>
             <Link href="/signup">
-              <Menu.Item header>
+              <Menu.Item
+                header
+                height="100%"
+                active={isActive("/signup")}
+                style={{ height: "100%" }}
+              >
                 <Icon name="signup" size="large" />
                 Signup
               </Menu.Item>
